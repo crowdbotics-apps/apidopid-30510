f9e412e05c2ec21f1c711b8113d5bc800c5dfb1e
Crowdbotics <team@crowdbotics.com>
2021-09-13 16:32:35 +0000

    Initial commit.

---
+++ backend/apidopid_30510/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for apidopid_30510 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apidopid_30510.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "apidopid_30510Identifier",
+  url: "https://apidopid_30510Identifier.botics.co",
+  api: "https://apidopid_30510Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
